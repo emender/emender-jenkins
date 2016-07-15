8533bfbef4eda65246804dfda04dd39b5da96b39
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-15 14:55:59 +0200

    Added basic library dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/tools.cli "0.3.1"]
+                   [ring/ring-core "1.3.2"]
+                   [ring/ring-jetty-adapter "1.3.2"]
+                   [org.clojure/data.json "0.2.5"]
+                   [clj-http "2.0.0"]
+                   [clj-jgit "0.8.0"]
+                   [clj-rm-rf "1.0.0-SNAPSHOT"]
+                   [clojure-ini "0.0.1"]
+                   [hiccup "1.0.4"]]
+                   )
