2095f528c45926501b56dbb09c6b14a6abec17f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-15 15:13:35 +0200

    Declaration of CLI options

--- src/emender_jenkins/core.clj
+++ src/emender_jenkins/core.clj
@@ -1,6 +1,29 @@
 (ns emender-jenkins.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require '[clojure.tools.cli       :as cli])
+ 
+(def cli-options
+    ;; an option with a required argument
+    [["-h"   "--help"                       "show help"                                                    :id :help]
+     ["-p"   "--port   PORT"                "port number on which Stage Dashboard should accepts requests" :id :port]
+     ["-j"   "--jenkins-url url"            "url to Jenkins, for example: http://10.20.30.40:8080/"        :id :jenkins-url]
+     ["-t"   "--test-jobs-suffix suffix"    "test jobs suffix, for example 'test'"                         :id :test-jobs-suffix]
+     ["-f"   "--fetch-only"                 "just start job fetcher once, then stop processing"            :id :fetch-only]
+     ["-c"   "--config"                     "just show the actual configuration"                           :id :show-config]])
+
+(defn -main
+    "Entry point to the titan server."
+    [& args]
+    (let [all-options         (cli/parse-opts args cli-options)
+          options             (all-options :options)
+          show-help?          (options :help)
+          fetch-only?         (options :fetch-only)
+          show-config?        (options :show-config)]
+          (System/setProperty "javax.net.ssl.keyStore",   "keystore")
+          (System/setProperty "javax.net.ssl.trustStore", "keystore")
+          (print-environment-configuration)
+          (cond show-help?   (show-help (:summary all-options))
+                fetch-only?  (fetch-jobs-only options)
+                show-config? (show-config options)
+                :else        (run-app options))))
+
