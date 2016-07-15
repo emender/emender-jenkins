b61c374394450e39b2a34bbb5679a91f63f28d84
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-15 14:58:39 +0200

    Configured plugins and dev libraries

--- project.clj
+++ project.clj
@@ -13,4 +13,16 @@
                    [clj-rm-rf "1.0.0-SNAPSHOT"]
                    [clojure-ini "0.0.1"]
                    [hiccup "1.0.4"]]
-                   )
+    :dev-dependencies [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.2"]]
+    :main ^:skip-aot emender-jenkins.core
+    :ring {:handler emender-jenkins.core/app
+           :ssl? true
+           :ssl-port 8443
+           :keystore "keystore"
+           :key-password "changeit"}
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
