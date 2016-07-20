b25427a965e84bafe2248b509df88b5fc2cf32a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-20 18:06:15 +0200

    Function to load configuration options from the INI file

--- src/emender_jenkins/config.clj
+++ src/emender_jenkins/config.clj
@@ -1,7 +1,27 @@
-(ns emender-jenkins.config)
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emender-jenkins.config
+    "Module that contains all functions required to to load configuration from the INI file.")
+
+(require '[clojure.pprint :as pprint])
+
+(require '[emender-jenkins.config-loader :as config-loader])
 
 (def default-port
     "Default port used when no -p or --port CLI option is specified."
     "3000")
 
+(defn load-configuration-from-ini
+    [filename]
+    (config-loader/load-configuration-file filename))
 
