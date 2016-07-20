ac839b7e9146ce358684e58680a691ec3c170f5d
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-20 17:57:18 +0200

    New module: middleware.clj

---
+++ src/emender_jenkins/middleware.clj
@@ -0,0 +1,23 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emender-jenkins.middleware
+    "Definition(s) of middleware used by this app with the cooperation with Clojure Ring.")
+
+(defn inject-configuration
+    "Inject configuration structure into the request parameter.
+     It means that the process request -> emender-jenkins -> response could be implemented
+     in a functional way."
+    [handler configuration]
+    (fn [request]
+        (handler (assoc request :configuration configuration))))
+
