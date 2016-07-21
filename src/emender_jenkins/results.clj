3a254e7cc972174d6991be5a662b8ddd1d7dda9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-21 12:46:44 +0200

    Module with functions to handling test results

---
+++ src/emender_jenkins/results.clj
@@ -0,0 +1,38 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emender-jenkins.results)
+
+(require '[clojure.pprint :as pprint])
+
+(require '[emender-jenkins.file-utils :as file-utils])
+
+(def results (atom {}))
+
+(defn render-edn-data
+    "Render EDN data to be used for debugging purposes etc."
+    [output-data pretty-print?]
+    (if pretty-print?
+        (with-out-str (pprint/pprint output-data))
+        (with-out-str (println output-data))))
+
+(defn add-new-results
+    [job-name new-results]
+    (swap! results assoc job-name new-results))
+
+(defn store-results
+    [pretty-print?]
+    (let [edn-data (render-edn-data @results pretty-print?)]
+        (spit "results2.edn" edn-data)
+        ; rename files atomically (on the same filesystem)
+        (file-utils/mv-file "results2.edn" "results.edn")))
+
