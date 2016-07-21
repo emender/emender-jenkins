b2dd09166bec5633c64e8cd869d4f963d92cda03
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-21 12:40:04 +0200

    Updated project settings: removed hiccup library (no longer needed), bumped versions of several other libraries

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject emender-jenkins "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.7.0"]
                    [org.clojure/tools.cli "0.3.1"]
                    [ring/ring-core "1.3.2"]
                    [ring/ring-jetty-adapter "1.3.2"]
                    [org.clojure/data.json "0.2.5"]
                    [clj-http "2.0.0"]
-                   [clj-jgit "0.8.0"]
+                   [clj-jgit "0.8.8"]
                    [clj-rm-rf "1.0.0-SNAPSHOT"]
                    [clojure-ini "0.0.1"]
-                   [hiccup "1.0.4"]]
+                   [irclj "0.5.0-alpha4"]]
     :dev-dependencies [[lein-ring "0.8.10"]]
     :plugins [[lein-ring "0.8.10"]
               [codox "0.8.11"]
               [test2junit "1.1.0"]
-              [lein-cloverage "1.0.2"]]
+              [lein-cloverage "1.0.6"]]
     :main ^:skip-aot emender-jenkins.core
     :ring {:handler emender-jenkins.core/app
            :ssl? true
@@ -26,3 +26,4 @@
            :key-password "changeit"}
     :target-path "target/%s"
     :profiles {:uberjar {:aot :all}})
+
