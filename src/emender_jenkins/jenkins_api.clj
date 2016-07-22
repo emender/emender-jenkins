8c99adf6cdb7d824e729f06956de7be38facd503
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-22 17:15:34 +0200

    First version of Jenkins API module

---
+++ src/emender_jenkins/jenkins_api.clj
@@ -0,0 +1,17 @@
+(ns emender-jenkins.jenkins-api)
+
+(require '[clojure.xml       :as xml])
+(require '[clojure.zip       :as zip])
+
+(require '[clojure.data.json :as json])
+(require '[clj-http.client   :as http-client])
+
+(defn read-list-of-all-jobs
+    [jenkins-url job-list-part]
+    (let [all-jobs-url (str jenkins-url job-list-part)]
+        (println "Using the following URL to retrieve all Jenkins jobs: " all-jobs-url)
+        (let [data (json/read-str (slurp all-jobs-url))]
+            (if data
+                (get data "jobs")
+                nil))))
+
