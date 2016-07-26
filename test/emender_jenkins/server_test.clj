2ae59ae3073c1451449ad317dd81e340306a3032
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-26 13:49:28 +0200

    Stub for the module server_test.clj

---
+++ test/emender_jenkins/server_test.clj
@@ -0,0 +1,18 @@
+(ns emender-jenkins.core-test
+  (:require [clojure.test :refer :all]
+            [emender-jenkins.server :refer :all]))
+
+
+;(get-api-command nil "/api")
+
+;(get-api-command "/" "/api")
+;(get-api-command "xyzzy/" "/api")
+;(get-api-command "xyzzy/xyzzy" "/api")
+
+;(get-api-command "/api" "/api")
+
+;(get-api-command "/api/create_job", "/api")
+
+;(get-api-command "/api/system/banners" "/api")
+
+
