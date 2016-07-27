838fdaf4751640c0be1f0683a67d8c6abcd00549
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-27 15:54:04 +0200

    Test scripts to check update job API

---
+++ test/api/update_job1.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book-en-US (test)", "url_to_repo":"new-url", "branch":"master"}' localhost:3000/api/update_job
+
---
+++ test/api/update_job3.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -X POST --header "Content-Type: application/json" -d '{"name":"unknown", "url_to_repo":"new-url", "branch":"other_branch"}' localhost:3000/api/update_job
+
