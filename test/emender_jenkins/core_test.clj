93d1920f8b60095c16bd2a897796e15d2d840792
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-27 12:05:17 +0200

    Stub for test module: core_test.clj + common test function

--- test/emender_jenkins/core_test.clj
+++ test/emender_jenkins/core_test.clj
@@ -1,7 +1,25 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns emender-jenkins.core-test
   (:require [clojure.test :refer :all]
             [emender-jenkins.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
