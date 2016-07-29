cd367675d02d8ba07962d582792824b82cda158d
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-29 13:03:50 +0200

    Added five tests into config-test module

--- test/emender_jenkins/config_test.clj
+++ test/emender_jenkins/config_test.clj
@@ -23,3 +23,37 @@
     [function-name]
     (clojure.test/function? function-name))
 
+;
+; Tests for various function definitions
+;
+
+(deftest test-update-configuration-existence
+    "Check that the emender-jenkins.config/update-configuration definition exists."
+    (testing "if the emender-jenkins.config/update-configuration definition exists."
+        (is (callable? 'emender-jenkins.config/update-configuration))))
+
+
+(deftest test-load-configuration-from-ini-existence
+    "Check that the emender-jenkins.config/load-configuration-from-ini definition exists."
+    (testing "if the emender-jenkins.config/load-configuration-from-ini definition exists."
+        (is (callable? 'emender-jenkins.config/load-configuration-from-ini))))
+
+
+(deftest test-assoc-in-if-not-nil-existence
+    "Check that the emender-jenkins.config/assoc-in-if-not-nil definition exists."
+    (testing "if the emender-jenkins.config/assoc-in-if-not-nil definition exists."
+        (is (callable? 'emender-jenkins.config/assoc-in-if-not-nil))))
+
+
+(deftest test-override-options-by-cli-existence
+    "Check that the emender-jenkins.config/override-options-by-cli definition exists."
+    (testing "if the emender-jenkins.config/override-options-by-cli definition exists."
+        (is (callable? 'emender-jenkins.config/override-options-by-cli))))
+
+
+(deftest test-print-configuration-existence
+    "Check that the emender-jenkins.config/print-configuration definition exists."
+    (testing "if the emender-jenkins.config/print-configuration definition exists."
+        (is (callable? 'emender-jenkins.config/print-configuration))))
+
+
