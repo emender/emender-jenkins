8d81e5e3ca05fa3887ff5c217561671869b882f7
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-29 13:09:05 +0200

    Added three tests into process_info_test.clj

--- test/emender_jenkins/process_info_test.clj
+++ test/emender_jenkins/process_info_test.clj
@@ -23,3 +23,19 @@
     [function-name]
     (clojure.test/function? function-name))
 
+
+;
+; Tests for various function definitions
+;
+
+(deftest test-get-current-pid-existence
+    "Check that the emender-jenkins.process-info/get-current-pid definition exists."
+    (testing "if the emender-jenkins.process-info/get-current-pid definition exists."
+        (is (callable? 'emender-jenkins.process-info/get-current-pid))))
+
+
+(deftest test-read-properties-existence
+    "Check that the emender-jenkins.process-info/read-properties definition exists."
+    (testing "if the emender-jenkins.process-info/read-properties definition exists."
+        (is (callable? 'emender-jenkins.process-info/read-properties))))
+
