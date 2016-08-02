5724147520dba32936a7931d405e5bb592e45865
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-02 14:36:00 +0200

    Updated another test scripts for get_job_results API call

--- test/api/get_job_results5.sh
+++ test/api/get_job_results5.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -X GET -v localhost:3000/api/get_job_results/doc-BPMS-6.3-Getting_New_Guide-en-US%20\(test\)
+curl -X GET -v localhost:3000/api/get_job_results/doc-BPMS-6.3-Getting_New_Guide-en-US%20\(test-preview\)
 
---
+++ test/api/get_job_results6.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -X GET -v localhost:3000/api/get_job_results/doc-BPMS-6.3-Getting_New_Guide-en-US%20\(test-stage\)
+
