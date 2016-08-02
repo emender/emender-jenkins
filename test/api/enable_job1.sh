7888f09d5235a37d241d6ab34bea0ec14e9e408d
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-02 15:05:55 +0200

    Test scripts for enabling test jobs for preview/prod/stage environmnents

--- test/api/enable_job1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d '{"name":"doc-Red_Hat_Certificate_System-10.0-Administration_Guide-en-US (test)"}' localhost:3000/api/enable_job
-
