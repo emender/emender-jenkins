85f313149f40a02313e0d7d833adbd38f929139c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-02 15:23:47 +0200

    Test scripts for creating test jobs for preview/prod/stage environmnents

---
+++ test/api/create_job1-stage.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book-en-US (test-stage)", "url_to_repo":"https://gitlab.cee.redhat.com/red-hat-certificate-system-documentation/doc-Red_Hat_Certificate_System-Administration_Guide.git", "branch":"master"}' localhost:3000/api/create_job
+
