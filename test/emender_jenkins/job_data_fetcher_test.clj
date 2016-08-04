e9e5ab1dab3daf8446887ff598877cffe848482d
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-04 11:37:59 +0200

    Tests for functions minutes-to-seconds, seconds-to-ms, and compute-sleep-amount

---
+++ test/emender_jenkins/job_data_fetcher_test.clj
@@ -0,0 +1,44 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emender-jenkins.job-data-fetcher-test
+  (:require [clojure.test :refer :all]
+            [emender-jenkins.job-data-fetcher :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+
+(deftest test-minutes-to-seconds-existence
+    "Check that the emender-jenkins.job-data_fetcher/minutes-to-seconds definition exists."
+    (testing "if the emender-jenkins.job-data_fetcher/minutes-to-seconds definition exists."
+        (is (callable? 'emender-jenkins.job-data_fetcher/minutes-to-seconds))))
+
+
+(deftest test-seconds-to-ms-existence
+    "Check that the emender-jenkins.job-data_fetcher/seconds-to-ms definition exists."
+    (testing "if the emender-jenkins.job-data_fetcher/seconds-to-ms definition exists."
+        (is (callable? 'emender-jenkins.job-data_fetcher/seconds-to-ms))))
+
+
+(deftest test-compute-sleep-amount-existence
+    "Check that the emender-jenkins.job-data_fetcher/compute-sleep-amount definition exists."
+    (testing "if the emender-jenkins.job-data_fetcher/compute-sleep-amount definition exists."
+        (is (callable? 'emender-jenkins.job-data_fetcher/compute-sleep-amount))))
+
+
