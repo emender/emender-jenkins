be14a58e61be48cb15a64b166f4b21c09f0217d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-04 11:41:06 +0200

    Added five tests intoo jenkins-api-test module

--- test/emender_jenkins/jenkins_api_test.clj
+++ test/emender_jenkins/jenkins_api_test.clj
@@ -23,3 +23,34 @@
     [function-name]
     (clojure.test/function? function-name))
 
+
+(deftest test-get-command-existence
+    "Check that the emender-jenkins.jenkins-api/get-command definition exists."
+    (testing "if the emender-jenkins.jenkins-api/get-command definition exists."
+        (is (callable? 'emender-jenkins.jenkins-api/get-command))))
+
+
+(deftest test-job-name->url-existence
+    "Check that the emender-jenkins.jenkins-api/job-name->url definition exists."
+    (testing "if the emender-jenkins.jenkins-api/job-name->url definition exists."
+        (is (callable? 'emender-jenkins.jenkins-api/job-name->url))))
+
+
+(deftest test-update-jenkins-url-existence
+    "Check that the emender-jenkins.jenkins-api/update-jenkins-url definition exists."
+    (testing "if the emender-jenkins.jenkins-api/update-jenkins-url definition exists."
+        (is (callable? 'emender-jenkins.jenkins-api/update-jenkins-url))))
+
+
+(deftest test-post-command-existence
+    "Check that the emender-jenkins.jenkins-api/post-command definition exists."
+    (testing "if the emender-jenkins.jenkins-api/post-command definition exists."
+        (is (callable? 'emender-jenkins.jenkins-api/post-command))))
+
+
+(deftest test-read-list-of-all-jobs-existence
+    "Check that the emender-jenkins.jenkins-api/read-list-of-all-jobs definition exists."
+    (testing "if the emender-jenkins.jenkins-api/read-list-of-all-jobs definition exists."
+        (is (callable? 'emender-jenkins.jenkins-api/read-list-of-all-jobs))))
+
+
