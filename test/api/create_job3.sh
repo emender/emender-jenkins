cfb1bbb856b234914ef63e6464da422bc737dc90
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-11 11:54:52 +0200

    Updated test script to create jobs to show HTTP status code

--- test/api/create_job3.sh
+++ test/api/create_job3.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book_2-en-US (test)", "url_to_repo":"https://gitlab.cee.redhat.com/red-hat-certificate-system-documentation/doc-Red_Hat_Certificate_System-Administration_Guide.git"}' localhost:3000/api/create_job
+curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book_2-en-US (test)", "url_to_repo":"https://gitlab.cee.redhat.com/red-hat-certificate-system-documentation/doc-Red_Hat_Certificate_System-Administration_Guide.git"}' localhost:3000/api/create_job
 
---
+++ test/api/create_job5.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -v -X POST localhost:3000/api/create_job
+
