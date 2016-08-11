eb3e50d19083d331c7276a3139eb8d62c26a462a
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-11 11:44:23 +0200

    Updated test script to start jobs to show HTTP status code

--- test/api/start_job1-preview.sh
+++ test/api/start_job1-preview.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d '{"name":"doc-Red_Hat_Certificate_System-10.0-Administration_Guide-en-US (test-preview)"}' localhost:3000/api/start_job
+curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Red_Hat_Certificate_System-10.0-Administration_Guide-en-US (test-preview)"}' localhost:3000/api/start_job
 
--- test/api/start_job2.sh
+++ test/api/start_job2.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d '{"name":"wrong-job"}' localhost:3000/api/start_job
+curl -v -X POST --header "Content-Type: application/json" -d '{"name":"wrong-job"}' localhost:3000/api/start_job
 
