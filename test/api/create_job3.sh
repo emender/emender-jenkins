5927ac1ec039f30a8ed4527d3eb834c489edc7db
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-12 17:44:04 +0200

    Updated test scripts to use proper json structure

--- test/api/create_job3.sh
+++ test/api/create_job3.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book_2-en-US (test)", "url_to_repo":"https://gitlab.cee.redhat.com/red-hat-certificate-system-documentation/doc-Red_Hat_Certificate_System-Administration_Guide.git"}' localhost:3000/api/create_job
+curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book_2-en-US (test)", "ssh_url_to_repo":"https://gitlab.cee.redhat.com/red-hat-certificate-system-documentation/doc-Red_Hat_Certificate_System-Administration_Guide.git"}' localhost:3000/api/create_job
 
--- test/api/update_job1-stage.sh
+++ test/api/update_job1-stage.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book-en-US (test-stage)", "url_to_repo":"new-url", "branch":"master"}' localhost:3000/api/update_job
+curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book-en-US (test-stage)", "ssh_url_to_repo":"new-url", "branch":"master"}' localhost:3000/api/update_job
 
