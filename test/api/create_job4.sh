51bc2e3905dd68c03809a04341cab51837683913
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-12 20:27:16 +0200

    Updated test scripts for create_job REST API call

--- test/api/create_job4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-curl -v -X POST --header "Content-Type: application/json" -d '{"name":"doc-Test_Product-1.0-Test_Book_3-en-US (test)", "branch":"master"}' localhost:3000/api/create_job
-
