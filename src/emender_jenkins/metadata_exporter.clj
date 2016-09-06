e18d357b850ce849084110687df88d4dbb488baa
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-09-06 13:20:00 +0200

    Function to export any data into JSON format

--- src/emender_jenkins/metadata_exporter.clj
+++ src/emender_jenkins/metadata_exporter.clj
@@ -10,15 +10,23 @@
 ;      Pavel Tisnovsky
 ;
 
-(ns emender-jenkins.metadata-exporter)
+(ns emender-jenkins.metadata-exporter
+    "Module for exporting selected metadata into selected format.")
 
-(require '[clojure.data.csv      :as csv])
+(require '[clojure.data.json :as json])
+(require '[clojure.xml       :as xml])
+(require '[clojure.data.csv  :as csv])
 
 (defn data->csv
     "Convert/format any data to CSV format."
     [data]
     (with-out-str
         (csv/write-csv *out* data)))
+
+(defn data->json
+    "Convert/format any data to JSON format."
+    [products]
+    (json/write-str products))
 
 (defn export2csv
     [columns data]
