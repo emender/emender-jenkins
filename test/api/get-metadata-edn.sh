d3b3cefaf96c3ac9cdef6e60f841a45be6a8db22
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-09-06 14:04:01 +0200

    Test scripts to get metadata in various formats

---
+++ test/api/get-metadata-edn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+#  (C) Copyright 2016  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+
+curl -X GET -v localhost:3000/api/get_metadata?format=edn -o out.edn
+
