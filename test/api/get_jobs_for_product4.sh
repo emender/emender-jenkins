e650623a951eed311d59b57ed52909c083e9a0bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-09-07 18:34:39 +0200

    Added proper (missing) licence text

--- test/api/get_jobs_for_product4.sh
+++ test/api/get_jobs_for_product4.sh
@@ -1,3 +1,13 @@
 #!/bin/env bash
+
+#
+#  (C) Copyright 2016  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+
 curl -X GET -v localhost:3000/api/get_jobs?product=Red%20Hat%20Enterprise%20Linux\&version=7
 
