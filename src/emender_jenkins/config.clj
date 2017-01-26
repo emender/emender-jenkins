5e7060d5920723b5e090571108102adf9b17a139
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-26 13:52:14 +0100

    Sleep amount could be real number

--- src/emender_jenkins/config.clj
+++ src/emender_jenkins/config.clj
@@ -32,7 +32,7 @@
         (update-in [:config :pretty-print]               utils/parse-boolean)
         (update-in [:config :include-jenkins-reply]      utils/parse-boolean)
         (update-in [:config :verbose-show-configuration] utils/parse-boolean)
-        (update-in [:fetcher :job-fetcher-delay]         utils/parse-int)
+        (update-in [:fetcher :job-fetcher-delay]         utils/parse-float)
         (update-in [:fetcher :run-job-fetcher]           utils/parse-boolean)
         (update-in [:irc :port]                          utils/parse-int)
         (update-in [:irc :connect]                       utils/parse-boolean)))
