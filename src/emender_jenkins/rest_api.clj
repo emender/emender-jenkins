6eb15fd428d56ae33811a39a36fa39f26751c228
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-16 11:53:40 +0100

    Refactoring: waive functions moved to its own module

--- src/emender_jenkins/rest_api.clj
+++ src/emender_jenkins/rest_api.clj
@@ -26,7 +26,7 @@
 (require '[emender-jenkins.metadata-exporter :as metadata-exporter])
 (require '[emender-jenkins.metadata-analyzer :as metadata-analyzer])
 (require '[emender-jenkins.process-info      :as process-info])
-(require '[emender-jenkins.utils             :refer :all])
+(require '[emender-jenkins.waive             :as waive])
 
 ; command names used by various REST API responses
 (def commands {
@@ -562,54 +562,13 @@
              (-> (create-bad-request-response "running_jobs" "Can not read Jenkins queue and/or selected view")
                  (send-error-response request :internal-server-error)))))
 
-(defn read-waive-input-data
-    [request]
-    (try
-        (-> request
-            read-request-body
-            (json/read-str :key-fn clojure.core/keyword))
-        (catch Exception e
-            (throw-exception "Can not read POST data: " e))))
-
-(defn parse-job-name
-    [input-data]
-    (let [job-name (:job_name input-data)]
-        (-> input-data
-            (assoc :product (results/job-name->product-name job-name))
-            (assoc :version (results/job-name->version job-name))
-            (assoc :guide   (results/job-name->book-name job-name))
-            (dissoc :job_name))))
-
-(defn assert-not-empty
-    [waive-data key-name message]
-    (if (empty? (get waive-data key-name))
-        (throw-exception message)))
-
-(defn check-waive-data
-    [waive-data]
-    (assert-not-empty waive-data :product    "Product name not specified")
-    (assert-not-empty waive-data :version    "Version not specified")
-    (assert-not-empty waive-data :guide      "Guide (book) name not specified")
-    (assert-not-empty waive-data :test_suite "Test suite not specified")
-    (assert-not-empty waive-data :test_name  "Test name not specified")
-    (assert-not-empty waive-data :cause      "Cause not specified")
-    (assert-not-empty waive-data :added      "Added (date) not specified"))
-
-(defn parse-waive-data
-    [input-data]
-    (let [parsed-data (if (:job_name input-data)
-                          (parse-job-name input-data)
-                          input-data)]
-         (check-waive-data parsed-data)
-         parsed-data))
-
 (defn waive
     "Waive one test result."
     [request]
     (try
-        (let [waive-input-data  (read-waive-input-data request)
-              parsed-waive-data (parse-waive-data waive-input-data)]
-              (send-response parsed-waive-data request))
+        (-> request
+            waive/waive-test
+            (send-response request))
         (catch Throwable e
             (let [error-message (str "Error waiving test results: " (.getMessage e))]
                 (log/error error-message)
---
+++ src/emender_jenkins/waive.clj
@@ -0,0 +1,69 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emender-jenkins.waive
+    "Test waiving functionality")
+
+(require '[clojure.data.json       :as json])
+(require '[clj-fileutils.fileutils :as file-utils])
+
+(require '[emender-jenkins.results :as results])
+(require '[emender-jenkins.utils   :refer :all])
+
+(defn read-waive-input-data
+    [request]
+    (try
+        (-> request
+            :body
+            file-utils/slurp-
+            (json/read-str :key-fn clojure.core/keyword))
+        (catch Exception e
+            (throw-exception "Can not read POST data: " e))))
+
+(defn parse-job-name
+    [input-data]
+    (let [job-name (:job_name input-data)]
+        (-> input-data
+            (assoc :product (results/job-name->product-name job-name))
+            (assoc :version (results/job-name->version job-name))
+            (assoc :guide   (results/job-name->book-name job-name))
+            (dissoc :job_name))))
+
+(defn assert-not-empty
+    [waive-data key-name message]
+    (if (empty? (get waive-data key-name))
+        (throw-exception message)))
+
+(defn check-waive-data
+    [waive-data]
+    (assert-not-empty waive-data :product    "Product name not specified")
+    (assert-not-empty waive-data :version    "Version not specified")
+    (assert-not-empty waive-data :guide      "Guide (book) name not specified")
+    (assert-not-empty waive-data :test_suite "Test suite not specified")
+    (assert-not-empty waive-data :test_name  "Test name not specified")
+    (assert-not-empty waive-data :cause      "Cause not specified")
+    (assert-not-empty waive-data :added      "Added (date) not specified"))
+
+(defn parse-waive-data
+    [input-data]
+    (let [parsed-data (if (:job_name input-data)
+                          (parse-job-name input-data)
+                          input-data)]
+         (check-waive-data parsed-data)
+         parsed-data))
+
+(defn waive-test
+    [request]
+        (let [waive-input-data  (read-waive-input-data request)
+              parsed-waive-data (parse-waive-data waive-input-data)]
+              parsed-waive-data))
+
