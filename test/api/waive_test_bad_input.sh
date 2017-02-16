21add55a8203ce348be544b3aa47e5cc712a7900
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-16 11:55:02 +0100

    Updated script to check test waiving

--- test/api/waive_test_bad_input.sh
+++ test/api/waive_test_bad_input.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d "bad*input" localhost:3000/api/waive
+curl -v -X POST --header "Content-Type: application/json" -d "bad*input" localhost:3000/api/waive
 echo
 
---
+++ test/api/waive_test_empty_post.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -v -X POST --header "Content-Type: application/json" -d "{}" localhost:3000/api/waive
+
