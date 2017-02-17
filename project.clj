8448ac928772a5ee0ed541b4ebcf59b91e500d7b
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-17 11:51:36 +0100

    Added SQL and JDBC libraries into project file

--- project.clj
+++ project.clj
@@ -16,8 +16,10 @@
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.7.0"]
+                   [org.clojure/java.jdbc "0.3.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [org.clojure/data.csv  "0.1.2"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
                    [ring/ring-core "1.3.2"]
                    [ring/ring-jetty-adapter "1.3.2"]
                    [org.clojure/data.json "0.2.5"]
