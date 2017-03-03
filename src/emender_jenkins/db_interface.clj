2fa6dee249768eb43b95b391dad081c097bec3b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-03 17:50:28 +0100

    Read waives - the 1st version

--- src/emender_jenkins/db_interface.clj
+++ src/emender_jenkins/db_interface.clj
@@ -149,7 +149,20 @@
 
 (defn read-waives
     [product version book test-case test-name]
-    )
+    (jdbc/query db-spec/emender-jenkins-db
+        ["
+select product_names.name, product_versions.version, guides.name, test_suites.name, tests.name, waives.cause
+    from waives
+         join tests            on waives.test_id = tests.id
+         join test_suites      on tests.test_suite_id = test_suites.id
+         join guides           on waives.guide_id = guides.id
+         join product_versions on  guides.prod_ver_id = product_versions.id
+         join product_names    on product_versions.product_id = product_names.id
+    where product_names.name = ?
+      and product_versions.version = ?
+      and guides.name = ?
+      and test_suites.name = ?
+      and tests.name       = ?" product version book test-case test-name]))
 
 ;(insert-test-waive {
 ;    :product    "Product name"
