1843b6eb4942f3f33058d7f124927873db7dffb6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-20 13:26:08 +0100

    Updated project settings - use clj-utils and clj-config-loader modules

--- project.clj
+++ project.clj
@@ -34,6 +34,8 @@
                                                       com.sun.jmdk/jmxtools
                                                       com.sun.jmx/jmxri]]
                    [org.slf4j/slf4j-log4j12 "1.6.6"]
+                   [org.clojars.tisnik/clj-utils "0.1.0-SNAPSHOT"]
+                   [org.clojars.tisnik/clj-config-loader "0.1.0-SNAPSHOT"]
                    [org.clojars.tisnik/clj-jenkins-api "0.10.0-SNAPSHOT"]
                    [org.clojars.tisnik/clj-calendar "0.3.0-SNAPSHOT"]
                    [org.clojars.tisnik/clj-fileutils "0.2.0-SNAPSHOT"]]
